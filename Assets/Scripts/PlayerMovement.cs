c2647ec1e9e3959b016696aa1bed885e5bd11218
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2020-11-04 13:35:57 -0800

    Added jump capabilities
    Also added the "ground" tag for game objects.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,26 +14,36 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("Player speed - left/right")]
     public float Speed = 5;
     [Tooltip("Amount of force the player jumps with")]
-    public float JumpForce = 1;
+    public float JumpForce = 500;
+    private Vector3 Jump;
     private float MoveInput;
 
     Rigidbody2D myRB;
 
     private bool FacingRight = true;
+    private bool OnGround = true;
+
 
     // Start is called before the first frame update
     void Start()
     {
         myRB = GetComponent<Rigidbody2D>();
+        Jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
-    
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         // MoveInput equals 1 if moving right, -1 if moving left
         MoveInput = Input.GetAxisRaw("Horizontal");
         myRB.velocity = new Vector2(MoveInput * Speed, myRB.velocity.y);
+
+        // Detect when player presses W (jump)
+        if(Input.GetKeyDown(KeyCode.W) && OnGround == true)
+        {
+            // Add an upward force
+            myRB.AddForce(Jump * JumpForce, ForceMode2D.Impulse);
+            OnGround = false;
+        }
 
         // Flip the player sprite if direction changes
         if(FacingRight == false && MoveInput > 0)
@@ -45,7 +55,6 @@ public class PlayerMovement : MonoBehaviour
             Flip();
         }
     }
-
     // Used to flip direction of player when direction change occurs
     void Flip()
     {
@@ -57,5 +66,14 @@ public class PlayerMovement : MonoBehaviour
         // Set the new scaler values
         transform.localScale = Scaler;
 
+    }
+    
+    // If the player collides with a GameObject w/ the "Ground" tag, reset their jump.
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            OnGround = true;
+        }
     }
 }

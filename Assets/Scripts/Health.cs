18865143c22630b641f74e62c4f4e18b6e035ca6
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2020-11-03 17:01:40 -0800

    Added scripts
    Added health & spawn scripts

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,51 @@
+﻿//////////////////////
+///Name: Andrew Dahlman-Oeth
+///Date: 11/3/2020
+///Description: Add to a Game Object to give it health. Includes the OnDeath function.
+//////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [Tooltip("The max health of the object")]
+    public float MaxHealth = 100;
+    [Tooltip("The current health of the object")]
+    public float CurrentHealth = 100;
+    [Tooltip("Whether or not to destroy object when object has no health")]
+    public bool DestroyAtZero = true;
+    public UnityEvent OnDeath;
+
+    public void ChangeHealth(int change)
+    {
+        CurrentHealth += change;
+
+        if(CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+
+        if(CurrentHealth <= 0)
+        {
+            CurrentHealth = 0;
+            OnDeath.Invoke();
+            if(DestroyAtZero)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,36 @@
+﻿//////////////////////
+///Name: Andrew Dahlman-Oeth
+///Date: 11/3/2020
+///Description: The spawn function is stored here to be used by Unity Events from other scripts.
+//////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFunction : MonoBehaviour
+{
+    [Tooltip("This is an array of game objects that spawn from spawn function")]
+    public GameObject[] ObjectsToSpawn;
+    // This function is built to be used by other scripts - requires public
+    public void Spawn()
+    {
+        for(int i = 0; i < ObjectsToSpawn.Length; ++i)
+        {
+            // Spawn each object here where the other object was
+            Instantiate(ObjectsToSpawn[i], transform.position, Quaternion.identity);
+            
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

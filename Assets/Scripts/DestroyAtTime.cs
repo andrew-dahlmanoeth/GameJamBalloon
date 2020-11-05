7b7db21e1bde66eff422db201fa53665ac17a5d4
sim-mcqueen <71671926+sim-mcqueen@users.noreply.github.com>
2020-11-05 13:53:23 -0800

    Added A jump sound

---
+++ Assets/Scripts/DestroyAtTime.cs
@@ -0,0 +1,43 @@
+﻿/////////////////////////
+//// Name: Sim
+//// Date: 10/21/2020
+//// Desc: Destroy Proj after some time has gone by
+/////////////////////////
+
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAtTime : MonoBehaviour
+{
+    [Tooltip("The time in seconds it takes for the projectile to despawn")]
+    public float DeathTime = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathTimer());
+    }
+
+    IEnumerator DeathTimer()
+    {
+
+
+        yield return new WaitForSeconds(DeathTime);
+        Destroy(gameObject);
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

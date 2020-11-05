b6e9218ae4df1c91c174014043175d670f150670
Voltare56 <67781912+Voltare56@users.noreply.github.com>
2020-11-05 12:59:26 -0800

    death scrpit for game
    press r to restart game

---
+++ Assets/Scripts/Restartlevel.cs
@@ -0,0 +1,21 @@
+﻿/////////////eddie zhao 
+/////////11/4/20
+//////////////descrpition: when the charter reaces the bottom restart the level
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restartlevel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))///how to active the code 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//// what will it do when the key is pressed 
+        } 
+        
+    }
+}

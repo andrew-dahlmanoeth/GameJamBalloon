d1578391f475de887f87a33cb1eebc4252c73a9f
melaniebaloban <melanie.baloban@digipen.edu>
2020-11-08 18:14:20 -0800

    Made the random enemy movement
    created a scene, placeholder, and movement script

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿//////////////////////////
+///Name: Melanie Baloban
+///Date: 11/8/20
+///Desc: Makes the enemy move in a random direction every 2 seconds
+//////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float waitTime = 0.5f;
+    public float minSpeed = 1f;
+    public float maxSpeed = 2f;
+    private Vector2 movement;
+    private float timeLeft;
+    Rigidbody2D myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if(timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timeLeft += waitTime;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        myRb.AddForce(movement * Random.Range(minSpeed, maxSpeed));
+    }
+
+}

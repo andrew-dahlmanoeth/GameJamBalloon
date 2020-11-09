722e621640dbdf28803f05eb5e749a90bdff448b
sim-mcqueen <71671926+sim-mcqueen@users.noreply.github.com>
2020-11-09 14:02:49 -0800

    Added A Way to change scenes while playing

---
+++ Assets/Scripts/MoveLevels.cs
@@ -0,0 +1,24 @@
+﻿//////////////////////////////////////////
+//// Name: Sim McQueen
+//// Date: 11/9
+//// Desc: Switches Scenes once touched with another object
+//////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveLevels : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+    }
+
+}

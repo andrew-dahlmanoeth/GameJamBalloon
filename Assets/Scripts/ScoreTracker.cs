81c09d8f3c56916d5a2d979608f5977409c1ea0c
sim-mcqueen <71671926+sim-mcqueen@users.noreply.github.com>
2020-11-10 17:53:58 -0800

    Start Working on Score Tracker

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public int score;
+    public int balloons;
+
+
+
+    void Start()
+    {
+        score = PlayerPrefs.GetInt("Player Score");
+
+
+    }
+
+    
+}

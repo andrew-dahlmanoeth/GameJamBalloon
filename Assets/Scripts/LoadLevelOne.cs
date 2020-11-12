dc085a125093a56f1f772a768e3b943cca3a5ecf
sim-mcqueen <71671926+sim-mcqueen@users.noreply.github.com>
2020-11-12 13:35:02 -0800

    Made the start button actually work

---
+++ Assets/Scripts/LoadLevelOne.cs
@@ -0,0 +1,18 @@
+﻿//////////////////////////////////////
+//// Name: Sim McQueen
+//// Date: 11/12
+//// Desc: A function to run when the start button is pressed
+//////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOne : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}

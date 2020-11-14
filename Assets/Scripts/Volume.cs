c6be12bed7c6f4808dc368605fdbe61f430054bc
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2020-11-13 16:25:28 -0800

    Audio slider

---
+++ Assets/Scripts/Volume.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    
+    public void SetVolume(float Volume)
+    {
+        mixer.SetFloat("MasterVol", Volume);
+    }
+}

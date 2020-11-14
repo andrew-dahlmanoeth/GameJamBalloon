3fb7bd35c08e496d0a5b40f7c3bc0c90829bbd3c
sim-mcqueen <71671926+sim-mcqueen@users.noreply.github.com>
2020-11-13 16:47:55 -0800

    you can die now

---
+++ Assets/Scripts/OnDeath.cs
@@ -0,0 +1,28 @@
+﻿//////////////////////////////////////
+//// Name: Sim McQueen
+//// Date: 11/13/20
+//// Desc: Runs when the character dies
+//////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnDeath : MonoBehaviour
+{
+    
+    
+
+    void Start()
+    {
+        StartCoroutine(whenPlayerDies());
+    }
+
+    IEnumerator whenPlayerDies()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}

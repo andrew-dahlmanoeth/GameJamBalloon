c0e80ac15afccd1fe108bbf0831272997cdfdaa6
adahlmanoeth <71671986+andrew-dahlmanoeth@users.noreply.github.com>
2020-11-13 17:27:28 -0800

    Fixed title screen

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject OptionsMenu;
+    public GameObject StartingMenu;
+
+    void Start()
+    {
+        OptionsMenu.SetActive(false);
+    }
+
+    public void Options()
+    {
+        StartingMenu.SetActive(false);
+        OptionsMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        StartingMenu.SetActive(true);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -12,4 +12,6 @@ public class Volume : MonoBehaviour
     {
         mixer.SetFloat("MasterVolume", Mathf.Log(Volume) * 20);
     }
+
+
 }
